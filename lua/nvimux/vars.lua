541ef05f8d258d5df59b361d94edce738c3ade78
Henry Kupty <hkupty@gmail.com>
2022-01-07 01:18:28 +0100

    Reorganize variables
    breaking: `quickterm_*` variables were moved to a `quickterm` table

--- lua/nvimux/vars.lua
+++ lua/nvimux/vars.lua
@@ -1,24 +1,36 @@
-local vars = {
-  prefix = '<C-b>',
-  local_prefix = {
+local vars = {}
+
+vars.local_prefix = {
     n = nil,
     v = nil,
     i = nil,
     t = nil
   },
-  vertical_split = ':NvimuxVerticalSplit',
-  horizontal_split = ':NvimuxHorizontalSplit',
-  quickterm_scope = 'g',
- quickterm_direction = 'botright',
-  quickterm_orientation = 'vertical',
-  quickterm_size = '',
-  quickterm_command = 'term',
-  close_term = ':x',
+
+vars.quickterm = {
+  scope = 'g',
+  direction = 'botright',
+  orientation = 'vertical',
+  size = '',
+  command = 'term',
 }
+
+vars.prefix = '<C-b>'
+-- Deprecated
+vars.vertical_split = ':NvimuxVerticalSplit'
+vars.horizontal_split = ':NvimuxHorizontalSplit'
+
+vars.close_term = function()
+    vim.api.nvim_buf_delete(0, {force = true})
+end
 
 vars.scratch_buf_content = {
   ""
 }
+
+vars.new_buffer = function()
+  return vim.api.nvim_create_buf(false, true)
+end
 
 vars.new_window = function()
     vim.api.nvim_set_current_buf(require("nvimux.ui").singleton_buf())
@@ -28,27 +40,8 @@ vars.new_tab = function()
     vim.api.nvim_set_current_buf(require("nvimux.ui").singleton_buf())
 end
 
-vars.split_type = function(t)
-  return t.quickterm_direction .. ' ' .. t.quickterm_orientation .. ' ' .. t.quickterm_size .. 'split'
+vars.quickterm.split_type = function(t)
+  return t.direction .. ' ' .. t.orientation .. ' ' .. t.size .. 'split'
 end
-
-local proto_vars = {}
-setmetatable(proto_vars, {
-    __index = function(_, k)
-      local v = rawget(vars, k)
-      if type(v) == "function" then
-        return v
-      else
-        return function()
-          return v
-        end
-      end
-    end,
-    __newindex = function(_, k, v)
-      rawset(vars, k ,v)
-    end
-  })
-
-vars.proto = proto_vars
 
 return vars
