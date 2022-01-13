3ccab233342cf82786c7b7e0ced9a51740043937
Henry Kupty <hkupty@gmail.com>
2022-01-13 17:31:57 +0100

    Remove unused functions

--- lua/nvimux/fns.lua
+++ lua/nvimux/fns.lua
@@ -1,24 +1,4 @@
 local fns = {}
-local nvim = vim.api -- luacheck: ignore
-
-fns.exists = function(var)
-  return nvim.nvim_call_function('exists', {var}) == 1
-end
-
-fns.clone = function(orig)
-    local orig_type = type(orig)
-    local copy
-    if orig_type == 'table' then
-        copy = {}
-        for orig_key, orig_value in next, orig, nil do
-            copy[fns.clone(orig_key)] = fns.clone(orig_value)
-        end
-        setmetatable(copy, fns.clone(getmetatable(orig)))
-    else -- number, string, boolean, etc
-        copy = orig
-    end
-    return copy
-end
 
 fns.prompt = function(message)
   vim.fn.inputsave()
