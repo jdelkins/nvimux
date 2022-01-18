0b80cdd9a2a473f02e16f8441e34b1b10f04b9f0
Henry Kupty <hkupty@gmail.com>
2022-01-18 11:29:45 +0100

    Special case bindings only on non <Cmd> rhs
    This allows to make use of <Cmd>

--- lua/nvimux/bindings.lua
+++ lua/nvimux/bindings.lua
@@ -4,7 +4,6 @@ local fns = {}
 
 local consts = {
   terminal_quit = '<C-\\><C-n>',
-  esc = '<ESC>',
 }
 
 if vim.keymap ~= nil then
@@ -40,26 +39,29 @@ bindings.keymap = function(binding, context)
     bindings.set_keymap(binding[1], context.prefix .. binding[2], binding[3], options)
   elseif (type(binding[3]) == "string") then
     local suffix = ''
+    local starts_with_colon = string.sub(binding[3], 1, 1) == ':'
 
     if binding.suffix == nil then
       -- TODO revisit
-      suffix = string.sub(binding[3], 1, 1) == ':' and '<CR>' or ''
+      suffix = starts_with_colon and '<CR>' or ''
     else
       suffix = binding.suffix
     end
 
-    if vim.tbl_contains(binding[1], 't') then
-      binding[1] = vim.tbl_filter(function(mode) return mode ~= 't' end, binding[1])
-      bindings.set_keymap('t',
-        context.prefix .. binding[2],
-        consts.terminal_quit .. binding[3] .. suffix,
-        options)
-    elseif vim.tbl_contains(binding[1], 'i') then
-      binding[1] = vim.tbl_filter(function(mode) return mode ~= 'i' end, binding[1])
-      bindings.set_keymap('i',
-        context.prefix .. binding[2],
-        consts.esc .. binding[3] .. suffix,
-        options)
+    if starts_with_colon then
+      if vim.tbl_contains(binding[1], 't') then
+        binding[1] = vim.tbl_filter(function(mode) return mode ~= 't' end, binding[1])
+        bindings.set_keymap('t',
+          context.prefix .. binding[2],
+          consts.terminal_quit .. binding[3] .. suffix,
+          options)
+      elseif vim.tbl_contains(binding[1], 'i') then
+        binding[1] = vim.tbl_filter(function(mode) return mode ~= 'i' end, binding[1])
+        bindings.set_keymap('i',
+          context.prefix .. binding[2],
+          consts.esc .. binding[3] .. suffix,
+          options)
+      end
     end
 
     bindings.set_keymap(binding[1], context.prefix .. binding[2], binding[3] .. suffix, options)
