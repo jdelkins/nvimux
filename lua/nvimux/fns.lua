6d079a4475b1a9cb29f7281910e24a51ab327d51
Henry Kupty <hkupty@gmail.com>
2022-01-19 10:28:06 +0100

    Remove unused functions
    Also, simplify the structure and create a new wrapper

--- lua/nvimux/fns.lua
+++ lua/nvimux/fns.lua
@@ -1,28 +1,23 @@
 local fns = {}
 
-fns.split = function(str)
-  local p = {}
-  for i=1, #str do
-    table.insert(p, str:sub(i, i))
-  end
-  return p
-end
-
-fns.build_cmd = function(options)
-  local nargs = options.nargs or 0
-  local cmd = options.cmd or options.lazy_cmd()
-
-  vim.cmd('command! -nargs=' .. nargs .. ' ' .. options.name .. ' ' .. cmd)
-end
-
-
 local function capitalize_first(s)
-	return string.upper(string.sub(s,1,1))..string.sub(s,2)
-end
-local function to_pascal(s)
-	return (string.gsub(capitalize_first(s),"_(%w+)",capitalize_first))
+  return string.upper(string.sub(s,1,1))..string.sub(s,2)
 end
 
-fns.snake_to_pascal = to_pascal
+fns.snake_to_pascal = function(s)
+  return (string.gsub(capitalize_first(s),"_(%w+)",capitalize_first))
+end
+
+fns.fn_or_command = function(cmd)
+  local tp = type(cmd)
+  if tp == "function" then
+    cmd()
+  elseif tp == "string" then
+    vim.cmd(cmd)
+  else
+    print("nvimux: Cannot run command of type " .. tp)
+  end
+end
+
 
 return fns
